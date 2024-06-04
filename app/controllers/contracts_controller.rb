3a2b1f7c93720c05831cbe5336540aae9eda6b45
Julien Paris <parisjulien87@gmail.com>
2024-06-04 18:12:36 +0200

    create contract

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -4,6 +4,15 @@ class ContractsController < ApplicationController
   end
 
   def create
-    
+    @contract = Contract.new(contract_params)
+    @contract.parent_id = current_user.id
+    @contract.save!
+    redirect_to root_path
+  end
+
+  private
+
+  def contract_params
+    params.require(:contract).permit(:nanny_id, :parent_id, :start_date, :end_date, :type, :gross_hourly_rate, :weekly_worked_hours)
   end
 end
