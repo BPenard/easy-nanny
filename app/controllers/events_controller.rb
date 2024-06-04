e73cad4276c0323731e88318df9499ae91b8c5eb
Jordan Jalabert <jordan.jalabert@gmail.com>
2024-06-05 00:12:39 +0200

    wip new event form and event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,24 @@ class EventsController < ApplicationController
     @events = policy_scope(Event)
     render 'welcome'
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.nanny_id = current_user.nanny.id
+    @event.children_id = current_user.children.id
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:nanny_id, :child_id, :date, :type)
+  end
 end
