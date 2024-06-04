8a791f2ad06ff26983de7b3a26ee0eb739392ae5
bastien penard <bastien.penard@gmail.com>
2024-06-04 12:47:44 +0200

    update of db migration files - foreign keys not working

--- db/migrate/20240603131352_create_contracts.rb
+++ db/migrate/20240603131352_create_contracts.rb
@@ -3,10 +3,15 @@ class CreateContracts < ActiveRecord::Migration[7.1]
     create_table :contracts do |t|
       t.date :start_date
       t.date :end_date
-      t.string :contract_type
+      t.string :type
       t.integer :weekly_worked_hours
+      t.float :gross_hourly_rate
+      t.references :nanny_id, null: false
+      t.references :parent_id, null: false
 
       t.timestamps
     end
+    add_foreign_key :contracts, :users, column: :nanny_id
+    add_foreign_key :contracts, :users, column: :parent_id
   end
 end
--- db/migrate/20240603135143_update_type_to_contracts.rb
+++
@@ -1,5 +0,0 @@
-class UpdateTypeToContracts < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :contracts, :contract_type, :type
-  end
-end
