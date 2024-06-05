b3402936bf50879e06b83f959a16bf1254909cfe
Jordan Jalabert <jordan.jalabert@gmail.com>
2024-06-05 12:19:38 +0200

    events form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,11 +12,12 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-    # @event.contract_id = current_user.contract.id
-    # @event.children_id = current_user.children.id
+    @event.contract = Contract.find(params[:event][:contract_id].to_i)
+    @event.child = Child.find(params[:event][:child].to_i)
     authorize @event
 
-    if @event.save
+    if @event.save!
+      raise
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
@@ -24,6 +25,6 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:contract_id, :child_id, :date, :type)
+    params.require(:event).permit(:contract_id, :child_id, :date, :photo)
   end
 end
