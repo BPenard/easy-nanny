f2ac0fb2aacea9da7f24d1904f621660bdc93895
diane johnston <diane.jroussillon@gmail.com>
2024-06-06 11:30:52 +0200

    commit

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,20 +1,26 @@
 class ContractsController < ApplicationController
+  include Stomp::Controller
+ 
   def new
-    @contract = Contract.new
+    @contract = build_record_for(Contract)
     authorize @contract
   end
 
   def create
     @contract = Contract.new(contract_params)
-    @contract.parent_id = current_user.id
-    @contract.save!
-    redirect_to root_path
-    authorize @contract
+    # @contract.parent_id = current_user.id
+    if params[:commit] == "create" && @contract.all_steps_valid?
+      @contract.save!
+      redirect_to contract_path @contract
+    else
+      @contract.step!(params[:commit])
+      redirect_to next_step_path_for(@contract, path: :new_contract_path)
+    end
   end
 
   private
 
   def contract_params
-    params.require(:contract).permit(:nanny_id, :parent_id, :start_date, :end_date, :type, :gross_hourly_rate, :weekly_worked_hours)
+    params.require(:contract).permit(:start_date, :end_date, :type, :gross_hourly_rate, :weekly_worked_hours, :serialized_steps_data)
   end
 end
--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,18 +1,29 @@
 class Contract < ApplicationRecord
   self.inheritance_column = nil
 
+  include Stomp::Model
+
+  stomp! validate: :each_step
+
   belongs_to :nanny, class_name: 'User', foreign_key: 'nanny_id'
   belongs_to :parent, class_name: 'User', foreign_key: 'parent_id'
-
+  
   has_many :payslips
   has_many :child_contracts
   has_many :children, through: :child_contracts
   has_many :events
 
-  validates :start_date, presence: true
+  define_steps step_1: [:start_date, :end_date], 
+             step_2: [:type, :weekly_worked_hours, :gross_hourly_rate]
+
+  define_step_validations step_2: {
+              type: { presence: true}
+            }       
+
+  # validates :start_date, presence: true
   # validates :end_date, presence: true # dans le cas d'un CDI pas de end date
-  validates :type, presence: true
-  validates :type, inclusion: { in: %w[cdi cdd] }
-  validates :weekly_worked_hours, presence: true
-  validates :gross_hourly_rate, presence: true
+  # validates :type, presence: true
+  # validates :type, inclusion: { in: %w[cdi cdd] }
+  # validates :weekly_worked_hours, presence: true
+  # validates :gross_hourly_rate, presence: true
 end
