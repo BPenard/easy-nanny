34dc36b6fca09befe8453d5493b3f72424844a4d
bastien penard <bastien.penard@gmail.com>
2024-06-12 00:34:50 +0200

    plein de modifs

--- app/javascript/controllers/welcome_controller.js
+++ app/javascript/controllers/welcome_controller.js
@@ -1,9 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
+const WEEK_DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+
+const today = new Date();
+let TARGETWEEKDAY = new Date(today);
 
 // Connects to data-controller="welcome"
 export default class extends Controller {
-  static targets = ["monday", "tuesday", "wednesday", "thursday", "friday","calendar"];
-  // static values = { day: String }
+  static targets = ["monday", "tuesday", "wednesday", "thursday", "friday","weekNum","calendar","monday-card", "tuesdayCard", "wednesdayCard", "thursdayCard", "fridayCard",];
+
+
 
   connect() {
     console.log("Hello from welcome controler");
@@ -22,40 +27,83 @@ export default class extends Controller {
     ];
     const dayName = days[dayOfWeek];
 
-    // On affiche les jours du jour même
-    const dayEvents = document.querySelectorAll(
-      `.contract-card[data-infos=${dayName}]`
-    );
-    dayEvents.forEach((dayEvent) => {
-      dayEvent.classList.remove("d-none");
-    });
+    this.weekNumTarget.innerText = `${TARGETWEEKDAY}`;
+
+
+    // On vérifie s'il y a des cards sur le jour, si oui on les affiche
+    if (this[`has${dayName.charAt(0).toUpperCase() + dayName.slice(1)}CardTarget`]) {
+      this[`${dayName}CardTarget`].classList.remove("d-none")
+    }
+
+    // pour chaque jour de la semaine s'il y à un event on retire la classe transparent
+    // et on affiche la class day-light
+    WEEK_DAYS.forEach((day) => {
+      if (this[`has${day.charAt(0).toUpperCase() + day.slice(1)}CardTarget`]) {
+        this[`${day}Target`].classList.remove("day-transparent")
+        this[`${day}Target`].classList.add("day-light")
+      }})
+
+
+    // On mets en marron le jour courant (et on retire les autres classes)
+    this[`${dayName}Target`].classList.remove("day-transparent")
+    this[`${dayName}Target`].classList.remove("day-light")
+    this[`${dayName}Target`].classList.add("day-strong")
+
   }
 
   events_of_day(event) {
+    // gestion des jours du calendrier
+
+    // pour chaque jour de la semaine
+      // 1. on retire la classe strong
+      // 2. s'il y à un event on met la classe day-light
+      // 3. sinon on attribut la classe day-transparent
+    WEEK_DAYS.forEach((day) => {
+      this[`${day}Target`].classList.remove("day-strong")
+
+      if (this[`has${day.charAt(0).toUpperCase() + day.slice(1)}CardTarget`]) {
+        this[`${day}Target`].classList.add("day-light")
+      } else {
+        this[`${day}Target`].classList.add("day-transparent")
+      }
+    })
+
+    // Puis on retire les classes non souhaités et on ajoute day-strong sur le jour choisi
+    this[`${event.currentTarget.dataset.day}Target`].classList.remove("day-transparent")
+    this[`${event.currentTarget.dataset.day}Target`].classList.remove("day-light")
+    this[`${event.currentTarget.dataset.day}Target`].classList.add("day-strong")
+
+
+    // gestion des cards
     // au click on masque tous les jours
-    const allEvents = document.querySelectorAll(`.contract-card`);
-    allEvents.forEach((event) => {
-      event.classList.add("d-none");
-    });
+
+    WEEK_DAYS.forEach((day) => {
+      if (this[`has${day.charAt(0).toUpperCase() + day.slice(1)}CardTarget`]) {
+        this[`${day}CardTarget`].classList.add("d-none")
+      }
+    })
 
     // puis on affiche les jours sur lequel on a cliqué
-    const dayEvents = document.querySelectorAll(
-      `.contract-card[data-infos=${event.currentTarget.dataset.day}]`
-    );
-    dayEvents.forEach((dayEvent) => {
-      dayEvent.classList.remove("d-none");
-    });
+    if (this[`has${event.currentTarget.dataset.day.charAt(0).toUpperCase() + event.currentTarget.dataset.day.slice(1)}CardTarget`]) {
+      this[`${event.currentTarget.dataset.day}CardTarget`].classList.remove("d-none")
+    }
   }
 
-  change_week() {
-    console.log("change week");
-    const today = new Date();
+  change_week(event) {
+
+    // const today = new Date();
+    // const targetWeekDate = new Date(today);
 
     // Reculer de 7 jours pour obtenir une date de la semaine précédente
-    const lastWeekDate = new Date(today);
-    lastWeekDate.setDate(today.getDate() - 7);
+    if (event.currentTarget.dataset.direction == "pastWeek"){
+      TARGETWEEKDAY.setDate(today.getDate() - 7);
+    } else if (event.currentTarget.dataset.direction == "futurWeek"){
+      TARGETWEEKDAY.setDate(today.getDate() + 7);
+    }
+    this.weekNumTarget.innerText = `${TARGETWEEKDAY}`;
 
-    const url = `/welcome?start_date=${lastWeekDate}`;
+
+    const url = `/welcome?start_date=${TARGETWEEKDAY}`;
 
     fetch(url, {
       method: "GET",
@@ -66,7 +114,7 @@ export default class extends Controller {
       .then((response) => response.json())
       .then((data) => {
         this.calendarTarget.innerHTML = data.content;
-        const dayOfPreviousWeek = lastWeekDate.getDay();
+        const dayOfPreviousWeek = TARGETWEEKDAY.getDay();
         const days = [
           "sunday",
           "monday",
@@ -76,14 +124,30 @@ export default class extends Controller {
           "friday",
           "saturday",
         ];
-        // const contract_card = document.querySelectorAll('.contract-card');
         const previousDayName = days[dayOfPreviousWeek];
-        console.log(previousDayName)
-        const dayEvents = document.querySelectorAll(`.contract-card[data-infos=${previousDayName}]`);
-        console.log(dayEvents)
-        dayEvents.forEach((dayEvent) => {
-          dayEvent.classList.remove("d-none");
-        });
+
+         // On vérifie s'il y a des cards sur le jour, si oui on les affiche
+        if (this[`has${previousDayName.charAt(0).toUpperCase() + previousDayName.slice(1)}CardTarget`]) {
+          this[`${previousDayName}CardTarget`].classList.remove("d-none")
+        }
+
+
+        WEEK_DAYS.forEach((day) => {
+          this[`${day}Target`].classList.remove("day-strong")
+          this[`${day}Target`].classList.remove("day-transparent")
+          this[`${day}Target`].classList.remove("day-light")
+
+          if (this[`has${day.charAt(0).toUpperCase() + day.slice(1)}CardTarget`]) {
+            this[`${day}Target`].classList.add("day-light")
+          } else {
+            this[`${day}Target`].classList.add("day-transparent")
+          }
+        })
+
+        this[`${previousDayName}Target`].classList.remove("day-transparent")
+        this[`${previousDayName}Target`].classList.remove("day-light")
+        this[`${previousDayName}Target`].classList.add("day-strong")
+
       })
       .catch((error) => {
         console.error(error);
