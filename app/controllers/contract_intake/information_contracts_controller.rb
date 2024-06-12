62a3f216cc98869f3fc51bbf33d43f97ea72f8e0
bastien penard <bastien.penard@gmail.com>
2024-06-12 10:36:19 +0200

    commit

--- app/controllers/contract_intake/information_contracts_controller.rb
+++ app/controllers/contract_intake/information_contracts_controller.rb
@@ -23,13 +23,24 @@ module ContractIntake
     private
 
     def set_information_contract
+      if information_params[:end_date].nil?
+        Raise
+        end_date = Date.parse(information_params[:end_date])
+      else
+        end_date = nil
+        Raise
+      end
+
       InformationContract.new(
         start_date: Date.parse(information_params[:start_date]),
-        end_date: Date.parse(information_params[:end_date]),
         weekly_worked_hours: information_params[:weekly_worked_hours],
+        end_date: end_date,
         type: information_params[:type],
         gross_hourly_rate: information_params[:gross_hourly_rate]
       )
+
+
+
     end
 
     def information_params
