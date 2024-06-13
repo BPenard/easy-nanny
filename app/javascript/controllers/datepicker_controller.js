6fb64613d8dde6aada7b73e20d9b4d496bf9084c
Julien Paris <parisjulien87@gmail.com>
2024-06-13 12:04:17 +0200

    fixes divers

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,8 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
+import { French } from "flatpickr/dist/l10n/fr.js"
+
 
 export default class extends Controller {
   connect() {
-    flatpickr(this.element, {});
+    flatpickr(this.element, {
+      locale: French
+    });
   }
 }
--- app/javascript/controllers/datepicker_with_time_controller.js
+++ app/javascript/controllers/datepicker_with_time_controller.js
@@ -1,11 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
+import { French } from "flatpickr/dist/l10n/fr.js"
 
 export default class extends Controller {
   connect() {
     flatpickr(this.element, {
       enableTime: true,
       dateFormat: "Y-m-d H:i",
+      locale: French
     });
   }
 }
